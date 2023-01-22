e8c168349e56e23e9bf562be3a4e46fdf5be6175
SwapnilYC <119727525+SwapnilYC@users.noreply.github.com>
2023-01-22 18:48:39 +0530

    string converted to lowercase

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 // complete the given function
 
-function palindrome(str){
-let arr = [...str];
-let	arrReverse = arr.reverse();
- for(let i = 0; i < arr.length; i++){
-	 if(arr[i] != arrReverse[i]){
-		 return false;
-	 }
- }
-	return true;
+function palindrome(str) {
+    str = str.toLowerCase();
+    let arr = [...str];
+    let arrReverse = arr.reverse();
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] != arrReverse[i]) {
+            return false;
+        }
+    }
+    return true;
 }
 module.exports = palindrome
