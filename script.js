2293bb099ffcef0f48c439b73599224272f02519
SwapnilYC <119727525+SwapnilYC@users.noreply.github.com>
2023-01-22 18:44:20 +0530

    code updated

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 // complete the given function
 
 function palindrome(str){
-
+let arr = [...str];
+let	arrReverse = arr.reverse();
+ for(let i = 0; i < arr.length; i++){
+	 if(arr[i] != arrReverse[i]){
+		 return false;
+	 }
+ }
+	return true;
 }
 module.exports = palindrome
