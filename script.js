1b7366054b3ef4b49e891908a15a4322e3f45471
SwapnilYC <119727525+SwapnilYC@users.noreply.github.com>
2023-01-22 19:06:00 +0530

    copy of main array is added and also loop over a string code also added

--- script.js
+++ script.js
@@ -2,13 +2,29 @@
 
 function palindrome(str) {
     str = str.toLowerCase();
+    // Using Spread operator on string
     let arr = [...str];
+    // console.log(arr);
+    let arrCopy = [...str];    // as reverse method also make changes to original array
     let arrReverse = arr.reverse();
-    for (let i = 0; i < arr.length; i++) {
-        if (arr[i] != arrReverse[i]) {
+    // console.log(`reverseArray:${arrReverse}`);
+    // console.log(`array:${arrCopy}`);
+
+     for (let i = 0; i < arr.length; i++) {
+        if (arrCopy[i] != arrReverse[i]) { 
             return false;
         }
     }
-    return true;
+
+    // Directly looping over a string
+    // var n = str.length;
+    // for (let i = 0; i < n / 2; i++) {
+    //     if (str[i] != str[n - i - 1]) { 
+    //         return false;
+    //     }
+    // }
+    // return true;
 }
+
+console.log(palindrome("ab"))
 module.exports = palindrome
